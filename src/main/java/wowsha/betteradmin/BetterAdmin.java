fa21ac4d52539c8ffb108d6be103c8cd40edef66
wowsha <pvanek216@gmail.com>
2026-09-04 17:47:50 +0200

    Add main Forge mod entrypoint

---
+++ src/main/java/wowsha/betteradmin/BetterAdmin.java
@@ -0,0 +1,32 @@
+package wowsha.betteradmin;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import wowsha.betteradmin.command.AdminCommands;
+import wowsha.betteradmin.util.TntManager;
+import wowsha.betteradmin.util.VanishManager;
+
+@Mod(BetterAdmin.MOD_ID)
+public class BetterAdmin {
+    public static final String MOD_ID = "better_admin";
+
+    public BetterAdmin() {
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(TntManager.class);
+        MinecraftForge.EVENT_BUS.register(VanishManager.class);
+    }
+
+    @SubscribeEvent
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        AdminCommands.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        TntManager.stopAll();
+        VanishManager.clear();
+    }
+}
